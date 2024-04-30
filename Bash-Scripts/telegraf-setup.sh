8a233e1284ef6934006fa150f2d651901eedb69b
kage-mo <168352543+kage-mo@users.noreply.github.com>
2024-04-30 08:51:34 +0200

    Delete Bash-Scripts directory

--- Bash-Scripts/telegraf-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Step 1: Install Telegraf
-sudo apt-get update
-sudo apt-get install -y telegraf
-sudo systemctl enable telegraf
-sudo systemctl start telegraf
-
-echo "Telegraf installed and started successfully."
